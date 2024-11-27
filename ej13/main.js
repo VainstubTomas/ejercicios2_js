db0a9404f4f9bd5801206c1b5e3f448142af8754
Tomas <vainstubtomas@gmail.com>
2024-11-27 14:48:34 -0300

    first commit

---
+++ ej13/main.js
@@ -0,0 +1,7 @@
+//Pide al usuario que ingrese un número utilizando prompt, luego verifica si es un número primo e imprime un mensaje correspondiente.
+
+let num = parseInt(prompt("Ingrese un numero entero"));
+
+//VALIDACION NROS PRIMOS
+//DIVISBLES POR 1 Y ELLOS MISMOS - Si tiene mas de dos divisores no es primo
+
